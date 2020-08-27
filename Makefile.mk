a15a635b822171682a924fecde3a17b0d2d3f675
aashna <aashnasharma3@gmail.com>
2020-08-27 13:45:21 -0400

    makefile

--- Makefile.mk
+++ Makefile.mk
@@ -1,13 +1,13 @@
 CFLAGS  := -fno-stack-protector -fno-builtin -nostdinc -O -g -Wall -I.
 LDFLAGS := -NOSTDLIB -Wl,-N -Wl,-Ttext -Wl,100000
 
-all:    kernel.bin
+all: kernel.bin
 
-kernel.bin:	start.o kmain.o
+kernel.bin: start.o kmain.o
 	ld -T link.ld -o kernel.bin start.o kmain.o
 	@echo Done.
 
-kernel2.bin: start.asm main.c start.o
+kernel2.bin: start.asm kmain.c start.o
 	gcc -o kernel.bin $(CFLAGS) start.o kmain.c $(LDFLAGS)
 
 start.o: start.asm
